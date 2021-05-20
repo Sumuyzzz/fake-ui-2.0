205a5a71502ea68afee0d5cbce70b57a69544e04
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-20 21:01:48 +0800

    💄 基本样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,58 @@
 <template>
-  <div :size="size">
-  <button v-bind="reset">
-    <slot />
-  </button>
-  </div>
+	<button class="fake-button" :class="{ [`theme-${theme}`]: theme }">
+		<slot />
+	</button>
 </template>
 
 <script>
-export default {
- inheritAttrs:false,
-  props:{
-
-  },
-  setup( props ,context){
-   const {size ,reset}=context=context.attrs;
-   return {size,reset}
-  }
-}
+	export default {
+		inheritAttrs: false,
+		props: {
+			theme: {
+				type: String,
+				default: "button",
+			},
+		},
+	};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+	$h: 32px;
+	$border-color: #d9d9d9;
+	$color: #333;
+	$blue: #40a9ff;
+	$radius: 4px;
+	.fake-button {
+		box-sizing: border-box;
+		height: $h;
+		padding: 0 12px;
+		cursor: pointer;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		white-space: nowrap;
+		background: white;
+		color: $color;
+		border: 1px solid $border-color;
+		border-radius: $radius;
+		box-shadow: 0 1px 0 fade-out(black, 0.95);
 
+		& + & {
+			margin-left: 8px;
+		}
+
+		&:hover,
+		&:focus {
+			color: $blue;
+			border-color: $blue;
+		}
+
+		&:focus {
+			outline: none;
+		}
+
+		&::-moz-focus-inner {
+			border: 0;
+		}
+	}
 </style>
