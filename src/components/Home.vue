01626a346201afd9c65119343f6e3ca3e4a45f8c
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-20 11:57:02 +0800

    ✨ 创建导航栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,14 @@
 <template>
 	<div>
-		<div class="topnav">
-			<div class="logo">LOGO</div>
-			<ul class="menu">
-				<li>菜单1</li>
-				<li>菜单2</li>
-			</ul>
-		</div>
-		<div class="banner">
-			<h1>轱辘UI</h1>
-			<h2>一个厉害的 UI 框架</h2>
-			<p class="actions">
-				<a>GitHub</a>
-				<a>开始</a>
-			</p>
-		</div>
+		<Topnav />
 	</div>
 </template>
+<script lang="ts">
+	import Topnav from "../components/Topnav.vue";
+
+	export default {
+		components: { Topnav },
+	};
+</script>
 <style lang="scss" scoped>
-	.topnav {
-		background: pink;
-		display: flex;
-		padding: 16px;
-		> .logo {
-			max-width: 6em;
-			margin-right: auto;
-		}
-		> .menu {
-			display: flex;
-			white-space: nowrap;
-			flex-wrap: nowrap;
-			> li {
-				margin: 0 1em;
-			}
-		}
-	}
 </style>
