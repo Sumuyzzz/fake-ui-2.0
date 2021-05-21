09f4fbf6d9777bef711cb433955f2d63c1a063ec
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-21 21:52:05 +0800

    ✨ Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,32 +1,38 @@
 <template>
-  <div>Dialog 示例</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">Toggle</Button>
-  <Dialog v-model:visible="x"></Dialog>
+	<div>Dialog 示例</div>
+	<h1>示例1</h1>
+	<Button @click="toggle">Toggle</Button>
+	<Dialog v-model:visible="x" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import {ref} from 'vue'
+	import Dialog from "../lib/Dialog.vue";
+	import Button from "../lib/Button.vue";
+	import { ref } from "vue";
 
-export default {
-  components: {
-    Dialog,
-    Button
-  },
-  setup() {
-    const x = ref(false)
-    const toggle = () => {
-      x.value = !x.value
-      console.log(x.value)
-    }
-    return {
-      x,
-      toggle
-    }
-  }
-}
+	export default {
+		components: {
+			Dialog,
+			Button,
+		},
+		setup() {
+			const x = ref(false);
+			const toggle = () => {
+				x.value = !x.value;
+				console.log(x.value);
+			};
+			const f1 = () => {
+				return false;
+			};
+			const f2 = () => {};
+			return {
+				x,
+				toggle,
+				f1,
+				f2,
+			};
+		},
+	};
 </script>
 
 <style>
