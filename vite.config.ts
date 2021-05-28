453c9ae1526a660adcbcfcb38d057a5b49396256
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-28 13:09:55 +0800

    🐛 配置修复

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // @ts-nocheck
-
-
-import { md } from "./plugins/md";
+import { md } from './src/plugins/MarkDownTransform'
 import fs from 'fs'
 import { baseParse } from '@vue/compiler-core'
 
 export default {
+	base: 'https://sumuyzzz.github.io/fakeUi-website-2.0/',
+	assetsDir: 'assets',
 	plugins: [md()],
 	vueCustomBlockTransforms: {
 		demo: (options) => {
@@ -15,10 +15,10 @@ export default {
 			const title = parsed.children[0].content
 			const main = file.split(parsed.loc.source).join('').trim()
 			return `export default function (Component) {
-        Component.__sourceCode = ${JSON.stringify(main)
+			Component.__sourceCode = ${JSON.stringify(main)
 				}
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
-      }`.trim()
+			Component.__sourceCodeTitle = ${JSON.stringify(title)}
+		}`.trim()
 		}
 	}
-};
+}
