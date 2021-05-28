ac6e24aa5ef98ba2fe9bd2479ff93db68c4a506b
Sumuyzzz <Sumuyzzz@gmail.com>
2021-05-28 16:59:23 +0800

    🐛 修改介绍文档路径错误的问题

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './view/Home.vue'
 import Document from './view/Document.vue'
-import { h } from 'vue'
 import MarkDown from './Document/MarkDown.vue'
 import SwitchDemo from './components/SwitchDemos/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemos/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemos/DialogDemo.vue'
 import TabsDemo from './components/TabsDemos/TabsDemo.vue'
-
+import { h } from 'vue'
+import Intro from './Document/markdown/intro.md'
+import Install from './Document/markdown/install.md'
+import Start from './Document/markdown/start.md'
 
 const history = createWebHashHistory()
-const md = filename => h(MarkDown, { path: `./markdown/${filename}.md`, key: filename })
+
+const md = string => h(MarkDown, { content: string, key: string })
 export const router = createRouter({
     history: history,
     routes: [
@@ -19,9 +22,9 @@ export const router = createRouter({
             path: '/doc', component: Document,
             children: [
                 { path: '', redirect: 'doc/intro' },
-                { path: 'intro', component: md('intro') },
-                { path: 'install', component: md('install') },
-                { path: 'start', component: md('start') },
+                { path: 'intro', component: md(Intro) },
+                { path: 'install', component: md(Install) },
+                { path: 'start', component: md(Start) },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,3 +3,10 @@ declare module '*.vue' {
     const Component: ComponentOptions
     export default Component
 }
+
+
+declare module '*.md' {
+    import { ComponentOptions } from 'vue'
+    const Component: ComponentOptions
+    export default Component
+}
